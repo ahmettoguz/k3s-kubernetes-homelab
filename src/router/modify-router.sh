6db295093318b8b5428bf07c40edf6833cc02c9a
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-11-28 19:15:32 +0300

    router

--- src/router/modify-router.sh
+++ src/router/modify-router.sh
@@ -2,3 +2,6 @@
 ip route del 10.42.1.0/24 dev flannel.1 onlink
 ip route del 10.42.2.0/24 dev flannel.1 onlink
 ip route del 10.42.3.0/24 dev flannel.1 onlink
+ip route add 10.42.1.0/24 via 192.168.56.33
+ip route add 10.42.2.0/24 via 192.168.56.34
+ip route add 10.42.3.0/24 via 192.168.56.32
