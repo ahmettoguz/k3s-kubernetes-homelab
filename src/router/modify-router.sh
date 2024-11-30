5f011362c132d04e75895227a53d202076595349
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-11-30 16:36:09 +0300

    modify route

--- src/router/modify-router.sh
+++ src/router/modify-router.sh
@@ -6,12 +6,12 @@ LOG_FILE="/var/log/modify-router.log"
 # Log the start of the script
 echo "$(date) - Script started" >> "$LOG_FILE"
 
-while ! ip route show | grep -q "10.42.2.0/24 dev flannel.1 onlink"; do
+while ! ip route show | grep -q "10.42.2.0/24 via 10.42.2.0 dev flannel.1 onlink"; do
     echo "$(date) - Waiting for route 10.42.2.0/24 dev flannel.1 onlink to exist..." >> "$LOG_FILE"
     sleep 1
 done
 
-ip route del 10.42.2.0/24 dev flannel.1 onlink
+ip route del 10.42.2.0/24 via 10.42.2.0 dev flannel.1 onlink
 ip route add 10.42.2.0/24 via 192.168.56.33 dev enp0s8 
 
 # Log the completion
